487a1c2d75e7ac19ab3d7442be9e70034a1c34c9
Carolina Cavalcanti <carolinapc@gmail.com>
2019-06-21 11:51:48 -0400

    new changes in the api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,25 @@ function searchArticles(){
     var numberRecords = $("#number-of-records").val().trim();
     var startYear = $("#start-year").val().trim();
     var endYear = $("#end-year").val().trim();
+    var nextYear = (new Date()).getFullYear() + 1;
 
-    var apiKey = "Dd1H4kApNLQLde9FMmEGHBaBG92kcTrn";
-     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+term+"&api-key="+apiKey;
-    //+"&count="+numberRecords+"&begin_date="+startYear+"&end_date="+endYear;
+    var queryParams = {"q": term, "api-key": "Dd1H4kApNLQLde9FMmEGHBaBG92kcTrn"};
+
+    if(startYear !== "" && endYear !== ""){
+        queryParams.fq = "pub_year:[" + startYear + " TO " + endYear + "]"; 
+    }
+    else if(startYear !== ""){
+        queryParams.fq = "pub_year:[" + startYear + " TO " + nextYear + "]"; 
+    }
+    else if(endYear !== ""){
+        queryParams.fq = "pub_year:[1900 TO " + endYear + "]"; 
+    }
+    else
+        queryParams.fq = "";
+    
+    
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + $.param(queryParams);
+    
     
     $.ajax({
         
